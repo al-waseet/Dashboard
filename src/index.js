ae2e0b2da89689d2bd6dd68c041fbe225e5127dc
German Shein <germanshein1995@gmail.com>
2022-12-13 20:37:25 +0400

    Fixing the environment recognition by adding a React-friendly Environmental Variable

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import Application from './Application';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot (document.getElementById ('root'));
-root.render (<React.StrictMode><Application /></React.StrictMode>);
+ReactDOM.createRoot (document.getElementById ('root')).render (<React.StrictMode><Application /></React.StrictMode>);
