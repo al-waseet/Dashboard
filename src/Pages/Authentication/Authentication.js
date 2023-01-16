34c4e4260e094d9b41efedc8a4ae02b6da37985b
German Shein <germanshein1995@gmail.com>
2023-01-16 18:18:24 +0400

    Authentication: Adding error states

--- src/Pages/Authentication/Authentication.js
+++ src/Pages/Authentication/Authentication.js
@@ -10,15 +10,23 @@ import { useNavigate } from 'react-router-dom';
 
 const Authentication = () => 
 {
-	const [Username, Set_Username] = useState ('');
 	const [Password, Set_Password] = useState ('');
+    const [Password_Field_Status, Set_Password_Field_Status] = useState ('');
 	const [Password_Reset_Mode, Set_Password_Reset_Mode] = useState (false);
     const [Password_Reset_Status, Set_Password_Reset_Status] = useState (false);
 	const [User, Set_User] = useContext (User_Context);
+    const [Username, Set_Username] = useState ('');
+    const [Username_Field_Status, Set_Username_Field_Status] = useState (false);
 	const Navigate = useNavigate ()
 
 	const Log_In = async () =>
 	{
+        if (Username === '' || Password === '')
+        {
+            Set_Username_Field_Status (!Username);
+            Set_Password_Field_Status (!Password);
+            return;
+        }
 		const Authenticated_User = await API.Log_In ({Username, Password});
 		Authenticated_User.Data.Avatar_File_Path = Authenticated_User.Data.Avatar;
 		Authenticated_User.Data.Avatar = Configuration.COS_URL + Authenticated_User.Data.Avatar;
@@ -52,7 +60,6 @@ const Authentication = () =>
 	{
 		//await API.Send_an_Email ({Username});
         Set_Password_Reset_Status (true);
-
 	}
 
 	return (
@@ -61,8 +68,8 @@ const Authentication = () =>
 				<a href='https://alwaseet.me' rel='noreferrer' target='_blank'><img alt='al waseet Banner' className='Authentication_Banner' src={al_waseet_Banner}></img></a>
 				{!Password_Reset_Status && 
                     <>
-                        <Text_Input_Field Label='Username' Function={Set_Username} Purpose="Username" Type="text"></Text_Input_Field>
-                        {!Password_Reset_Mode && <Text_Input_Field Label='Password' Function={Set_Password} Purpose="Password" Type="password"></Text_Input_Field>}
+                        <Text_Input_Field Error_Message='Please enter your username' Error_Status={Username_Field_Status} Label='Username' Function={Set_Username} Purpose="Username" Type="text"></Text_Input_Field>
+                        {!Password_Reset_Mode && <Text_Input_Field Error_Message='Please enter your password' Error_Status={Password_Field_Status} Label='Password' Function={Set_Password} Purpose="Password" Type="password"></Text_Input_Field>}
                         <div className='Forgot_Password_Field' onClick={() => Set_Password_Reset_Mode (!Password_Reset_Mode)}>{!Password_Reset_Mode ? 'Forgot the password?' : 'Remembered the password?'}</div>
                         <Button Function={Password_Reset_Mode ? Send_a_Link_to_Reset_the_Password : Log_In} Text={Password_Reset_Mode ? "Reset" : "Log In"}></Button>
                     </>
