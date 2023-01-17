68797334f07a374fef6048465a91935f5df6762c
German Shein <germanshein1995@gmail.com>
2023-01-17 19:46:46 +0400

    Authentication Page: Error UX Finalization

--- src/Components/Text_Input_Field/Text_Input_Field.js
+++ src/Components/Text_Input_Field/Text_Input_Field.js
@@ -1,11 +1,11 @@
 import {ReactComponent as Error_Icon} from '../../Images/Error_Icon.svg';
 import './Text_Input_Field.css';
 
-const Text_Input_Field = ({Bold_Status, Color, Disabled_Status, Error_Message, Error_Status, Function, Label, Type, Value}) => 
+const Text_Input_Field = ({Bold_Status, Color, Disabled_Status, Error_Message, Error_Status, Function, Label, Set_Error_Status, Type, Value}) => 
 <div className="Text_Input">
     {Label && <label className='Text_Input_Label' style={{color: Color}}>{Label}</label>}
     {Error_Status && <h5 className='Error_Message'><Error_Icon className='Error_Icon' fill='#AC0202'></Error_Icon>{Error_Message}</h5>}
-    <input className={`Text_Input_Field ${Error_Status ? 'Text_Input_Error' : null}`} disabled={Disabled_Status} onChange={(Event) => Function (Event.target.value)} style={{borderColor: Color, color: Color, fontWeight: Bold_Status ? 'bold' : 'normal'}} type={Type} value={Value} />
+    <input className={`Text_Input_Field ${Error_Status ? 'Text_Input_Error' : null}`} disabled={Disabled_Status} onChange={(Event) => Function (Event.target.value)} onClick={() => Set_Error_Status (false)} style={{borderColor: Color, color: Color, fontWeight: Bold_Status ? 'bold' : 'normal'}} type={Type} value={Value} />
 </div>
 
 export default Text_Input_Field;
--- src/Pages/Authentication/Authentication.js
+++ src/Pages/Authentication/Authentication.js
@@ -68,8 +68,8 @@ const Authentication = () =>
 				<a href='https://alwaseet.me' rel='noreferrer' target='_blank'><img alt='al waseet Banner' className='Authentication_Banner' src={al_waseet_Banner}></img></a>
 				{!Password_Reset_Status && 
                     <>
-                        <Text_Input_Field Error_Message='Please enter your username' Error_Status={Username_Field_Status} Label='Username' Function={Set_Username} Purpose="Username" Type="text"></Text_Input_Field>
-                        {!Password_Reset_Mode && <Text_Input_Field Error_Message='Please enter your password' Error_Status={Password_Field_Status} Label='Password' Function={Set_Password} Purpose="Password" Type="password"></Text_Input_Field>}
+                        <Text_Input_Field Error_Message='Please enter your username' Error_Status={Username_Field_Status} Label='Username' Function={Set_Username} Purpose="Username" Set_Error_Status={Set_Username_Field_Status} Type="text"></Text_Input_Field>
+                        {!Password_Reset_Mode && <Text_Input_Field Error_Message='Please enter your password' Error_Status={Password_Field_Status} Label='Password' Function={Set_Password} Purpose="Password" Set_Error_Status={Set_Password_Field_Status} Type="password"></Text_Input_Field>}
                         <div className='Forgot_Password_Field' onClick={() => Set_Password_Reset_Mode (!Password_Reset_Mode)}>{!Password_Reset_Mode ? 'Forgot the password?' : 'Remembered the password?'}</div>
                         <Button Function={Password_Reset_Mode ? Send_a_Link_to_Reset_the_Password : Log_In} Text={Password_Reset_Mode ? "Reset" : "Log In"}></Button>
                     </>
